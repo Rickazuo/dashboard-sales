6bc3a6843e16b0b3faedd496f8ad74e5bb296a12
Rickazuo <rick3209@gmail.com>
2023-02-24 18:08:16 -0300

    add overlay mask and add style popup

--- src/components/popup/popup.jsx
+++ src/components/popup/popup.jsx
@@ -1,11 +1,27 @@
 import styles from "./styles.module.css";
 
-const Popup = ({ isOpen, close }) => {
+const Popup = ({ close }) => {
   debugger;
   return (
-    <div className={styles.container}>
-      diashdusahdas
-      <button onClick={close}></button>
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <div className={styles.inputsDatas}>
+          <div className={styles.title}>Adicione os Dados</div>
+          <div className={styles.finishedSalesContainer}>
+            <div>Vendas Fechadas</div>
+            <input placeholder="Quantidade Esperada" type="text" />
+            <input placeholder="Quantidade Alcançada" type="text" />
+          </div>
+          <div className={styles.monthGoalContainer}>
+            <div>Meta Mensal</div>
+            <input placeholder="Valor Esperado" type="text" />
+            <input placeholder="Valor Alcançado" type="text" />
+          </div>
+          <button className={styles.buttonOverLay} onClick={close}>
+            Aplicar Dados
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
