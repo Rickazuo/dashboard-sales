7b4f44e550aaccbf0845274e95f9c43289b49ea5
Júlia Acosta <juliagabriela98@gmail.com>
2023-02-25 16:58:01 -0300

    fix funcionality of weekSales

--- src/components/columnChart/columnChart.jsx
+++ src/components/columnChart/columnChart.jsx
@@ -1,83 +1,87 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Chart from "react-apexcharts";
 
-const ColumnChart = ({ width }) => {
-  const [series] = useState([
-    {
-      name: "Sales",
-      data: [44, 55, 57, 56, 61, 58, 63],
-    },
-  ]);
-  const [options] = useState({
-    chart: {
-      type: "bar",
-      toolbar: {
-        show: false,
-      },
-      foreColor: "#ffffff",
-    },
-    colors: ["#90F7EC", "#32CCBC"],
-    grid: {
-      show: false,
-    },
-    plotOptions: {
-      bar: {
-        horizontal: false,
-        columnWidth: "40%",
-        endingShape: "rounded",
-        borderRadius: 12,
-      },
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    stroke: {
-      show: true,
-      width: 7,
-      colors: ["#363447"],
-    },
-    xaxis: {
-      categories: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
-      colors: ["#363447"],
-      axisTicks: {
-        show: false,
-      },
-    },
-    yaxis: {
-      enabled: false,
-      labels: {
-        show: false,
-        style: {
-          colors: [],
-          fontSize: "12px",
-          fontFamily: "Inter",
-          fontWeight: 400,
+const ColumnChart = ({ width, weeklyResults }) => {
+    const [series, setSeries] = useState([
+        {
+            name: "Sales",
+            data: weeklyResults.weeklySales,
         },
-      },
-    },
-    fill: {
-      opacity: 1,
-    },
-    tooltip: {
-      y: {
-        formatter: function (val) {
-          return "R$ " + val + " K";
+    ]);
+    const [options] = useState({
+        chart: {
+            type: "bar",
+            toolbar: {
+                show: false,
+            },
+            foreColor: "#ffffff",
         },
-      },
-    },
-  });
+        colors: ["#90F7EC", "#32CCBC"],
+        grid: {
+            show: false,
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: "40%",
+                endingShape: "rounded",
+                borderRadius: 12,
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            show: true,
+            width: 7,
+            colors: ["#363447"],
+        },
+        xaxis: {
+            categories: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            colors: ["#363447"],
+            axisTicks: {
+                show: false,
+            },
+        },
+        yaxis: {
+            enabled: false,
+            labels: {
+                show: false,
+                style: {
+                    colors: [],
+                    fontSize: "12px",
+                    fontFamily: "Inter",
+                    fontWeight: 400,
+                },
+            },
+        },
+        fill: {
+            opacity: 1,
+        },
+        tooltip: {
+            y: {
+                formatter: function (val) {
+                    return "R$ " + val + " K";
+                },
+            },
+        },
+    });
 
-  return (
-    <div className="chart">
-      <Chart
-        options={options}
-        series={series}
-        type="bar"
-        width={width < 400 ? "360" : "628"}
-        height="300"
-      />
-    </div>
-  );
+    useEffect(() => {
+        setSeries({ ...series, data: weeklyResults.weeklySales });
+    }, [weeklyResults]);
+
+    return (
+        <div className="chart">
+            <Chart
+                options={options}
+                series={series}
+                type="bar"
+                width={width < 400 ? "360" : "628"}
+                height="300"
+            />
+        </div>
+    );
 };
 
 export default ColumnChart;
